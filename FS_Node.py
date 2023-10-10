d9ef20d8c3f9484f6c3b4c324c5f0a0d0442fbf1
Gustominox <agstcmps.111@gmail.com>
2023-10-10 17:38:57 +0100

    Add END TRACKER and FILES LIST option, all option working

--- FS_Node.py
+++ FS_Node.py
@@ -25,18 +25,35 @@ class FS_Node:
         except:
             print("Impossivel Conectar")
         
-        # msg = soc.recv(1024)
-        # line = msg.decode('utf-8')
-
+        
         soc.close()
             
         print("CLOSING SOCKET")    
 
+    def askForList(self, sendAdress = "127.0.0.1"):
+        soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
+                          socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
+
+        soc.connect((sendAdress, self.porta))
+
+        try:
+            soc.sendall("FILES LIST".encode('utf-8'))
+        except:
+            print("Impossivel Conectar")
         
+        msg = soc.recv(1024)
+        line = msg.decode('utf-8')
+        print(line)
+        soc.close()
+            
+        print("CLOSING SOCKET")  
 def main():
     
     node = FS_Node()
-    node.updateEntrys() 
+    if sys.argv[1] == "FILES LIST":
+        node.askForList()
+    else:
+        node.updateEntrys() 
     
     
 if __name__ == "__main__":
