b1dd63f251a664fa64a0da7c2e5cea2187fd8633
Gustominox <agstcmps.111@gmail.com>
2023-10-10 10:21:34 +0100

    TCP Connection

--- FS_Node.py
+++ FS_Node.py
@@ -1,16 +1,43 @@
-class FS_NODE:
+import socket
 
-    def __init__(self, conf_file, port = 9090):
+class FS_Node:
+
+    def __init__(self, port = 9090):
 
         # self.startTime = datetime.now()
 
-        self.conf = Config(conf_file)
-
-        # innit_log(self.conf.logFile)
 
 
-        # self.hostname = socket.gethostname()
-        self.endereco = socket.gethostbyname(self.hostname)  # '10.0.0.1'
+
+        self.hostname = socket.gethostname()
+        self.endereco = socket.gethostbyname(self.hostname)  
         self.porta = port
 
+    def startTCP(self, sendAdress = "127.0.0.1"):
+        
+        soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
+                          socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
+
+        soc.connect((sendAdress, self.porta))
+
+        try:
+            soc.sendall("REGIST FS_NODE".encode('utf-8'))
+
+        except:
+            debug("Impossivel Conectar")
+        
+        msg = soc.recv(1024)
+        line = msg.decode('utf-8')
+
+        soc.close()
+            
+        print("CLOSING SOCKET")    
+
+        
+def main():
     
+    tracker = FS_Tracker()
+    tracker.FS_TRACK_PROTOCOL() 
+    
+if __name__ == "__main__":
+    main()
