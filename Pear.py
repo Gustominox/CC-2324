93810bf32db286193f16014a399c3518bf4a49e1
Gustominox <agstcmps.111@gmail.com>
2023-10-10 09:47:11 +0100

    Changes to project struct

---
+++ Pear.py
@@ -0,0 +1,15 @@
+class Pear:
+
+    def __init__(self, conf_file, port = 9090):
+
+        # self.startTime = datetime.now()
+
+        self.conf = Config(conf_file)
+
+        # innit_log(self.conf.logFile)
+
+
+        # self.hostname = socket.gethostname()
+        self.endereco = socket.gethostbyname(self.hostname)  # '10.0.0.1'
+        self.porta = port
+
