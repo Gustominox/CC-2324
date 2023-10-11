53f2cf019f6867f0e65fc12b7ef3d3196335e2d4
Gustominox <agstcmps.111@gmail.com>
2023-10-11 10:35:44 +0100

    Decoding TCP msg, starting FS TRACK PROTOCOL sintax

--- FS_Node.py
+++ FS_Node.py
@@ -1,60 +1,87 @@
 import socket
 import sys
+
+message = """# HEADER
+UPDATE NODE
+# BODY
+FILE1 SEG=[...] SIZE
+FILE2 SEG=[...] SIZE
+FILE3 SEG=[...] SIZE """
+
+
 class FS_Node:
 
-    def __init__(self, port = 9090):
+    def __init__(self, port=9090):
 
         # self.startTime = datetime.now()
 
-
-
-
         self.hostname = socket.gethostname()
-        self.endereco = socket.gethostbyname(self.hostname)  
+        self.endereco = socket.gethostbyname(self.hostname)
         self.porta = port
 
-    def updateEntrys(self, sendAdress = "127.0.0.1"):
-        
-        soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
-                          socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
+    def updateEntrys(self):
 
-        soc.connect((sendAdress, self.porta))
+        soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
+                            socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
 
         try:
-            soc.sendall(sys.argv[1].encode('utf-8'))
+            soc.connect((self.endereco, self.porta))
+            soc.sendall(message.encode('utf-8'))
+        except:
+            print("Impossivel Conectar")
+
+        soc.close()
+
+        print("CLOSING SOCKET")
+    
+    def sendTcpMsg(self,msg):
+
+        soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
+                            socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
+        try:
+            soc.connect((self.endereco, self.porta))
+            print(msg)
+            
+            soc.sendall(msg.encode('utf-8'))
         except:
             print("Impossivel Conectar")
         
-        
         soc.close()
-            
-        print("CLOSING SOCKET")    
 
-    def askForList(self, sendAdress = "127.0.0.1"):
+        print("CLOSING SOCKET")
+
+        
+    def askForList(self):
         soc = socket.socket(socket.AF_INET,     # Familia de enderecos ipv4
-                          socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
-
-        soc.connect((sendAdress, self.porta))
+                            socket.SOCK_STREAM)  # Connection-Oriented (TCP PROTOCOL)
 
         try:
+            soc.connect((self.endereco, self.porta))
             soc.sendall("FILES LIST".encode('utf-8'))
         except:
             print("Impossivel Conectar")
-        
+
         msg = soc.recv(1024)
         line = msg.decode('utf-8')
         print(line)
         soc.close()
-            
-        print("CLOSING SOCKET")  
+
+        print("CLOSING SOCKET")
+
+
 def main():
+
     
     node = FS_Node()
     if sys.argv[1] == "FILES LIST":
         node.askForList()
+    elif sys.argv[1] == "UPDATE NODE":
+        node.sendTcpMsg(message)
+    elif sys.argv[1] == "END TRACKER":
+        node.sendTcpMsg("END TRACKER")
     else:
-        node.updateEntrys() 
-    
-    
+        print("ERROR: Unknown command")
+
+
 if __name__ == "__main__":
     main()
