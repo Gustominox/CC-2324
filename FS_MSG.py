79a3a9d294446d771cadd829f7a93b2cc2a9a1df
Gustominox <agstcmps.111@gmail.com>
2023-10-17 16:51:23 +0100

    Parsing FS TRACK PROTOCOL message

--- FS_MSG.py
+++ FS_MSG.py
@@ -1,28 +1,49 @@
-class FS_MSG:
+class FS_Msg:
     def __init__(self):
-        self.SENDER_ID = None  # SENDER_ID
-        self.MSG_TYPE = None 
-        self.BODY = {}
+        self.SENDER_ID = "NO VALUE"  
+        self.SENDER_IP = "NO VALUE"  
+        self.MSG_TYPE = "NO VALUE"
+        self.BODY = "NO VALUE"
+
     def __str__(self):
-        return
-    
+
+        out = ""
+        out += "Message = { \n"
+        out += f"SENDER_ID = {self.SENDER_ID}\n"
+        out += f"SENDER_IP = {self.SENDER_IP}\n"
+        out += f"MSG_TYPE = {self.MSG_TYPE}\n"
+        out += f"BODY = {self.BODY}\n"
+        out += "}"
+
+        return out
+
     def __repr__(self):
-        return
-    
-    def read_message(self,data):
-        print(data)
-        
-        for line in data:
-            line.split(";")
-            for field in line:
-                field.split("=")
-                
-                if field[0] == "SENDER_ID":
-                    self.SENDER_ID = field[1]
-                elif field[0] == "MSG_TYPE":
-                    self.MSG_TYPE = field[1]
-                elif field[0] == "BODY":
-                    body = ''.join(field[1].splitlines().strip("\{\} ").split(","))
-                    
-                else:
-                  pass
+
+        out = ""
+        out += "Message = { \n"
+        out += f"SENDER_ID = {self.SENDER_ID}\n"
+        out += f"SENDER_IP = {self.SENDER_IP}\n"
+        out += f"MSG_TYPE = {self.MSG_TYPE}\n"
+        out += f"BODY = {self.BODY}\n"
+        out += "}"
+
+        return out
+
+    def read_message(self, data):
+        # print(f"DATA IN MSG: {data}")
+        print(f"DATA: {data}")
+
+        for field in data.split(";"):
+            element = field.split("=")
+            # print(element)
+            if element[0] == "SENDER_ID":
+                self.SENDER_ID = element[1]
+            if element[0] == "SENDER_IP":
+                self.SENDER_IP = element[1]
+            elif element[0] == "MSG_TYPE":
+                self.MSG_TYPE = element[1]
+            elif element[0] == "BODY":
+                self.BODY = element[1].strip("\{\} ").split(",")
+
+            else:
+                pass
