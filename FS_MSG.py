79ca885fd5a32d16d742ff7c3d935a5c49da08d6
Gustominox <agstcmps.111@gmail.com>
2023-10-17 10:13:25 +0100

    Start MSG module

--- FS_MSG.py
+++ FS_MSG.py
@@ -1,7 +1,13 @@
 class FS_MSG:
     def __init__(self):
-        self.SENDER_ID = None  # Inicia o campo SENDER_ID
-        self.MSG_TYPE = None # Inicia o nodo vazio
+        self.SENDER_ID = None  # SENDER_ID
+        self.MSG_TYPE = None 
+        self.BODY = {}
     def __str__(self):
         return
     
+    def __repr__(self):
+        return
+    
+    def read_message(self,data):
+        
