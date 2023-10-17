cb856ad1f9b1f15175e87ba9421dc1883fbcf638
Gustominox <agstcmps.111@gmail.com>
2023-10-17 17:54:40 +0100

    Changes to table, reorganizing how the info is saved

--- FS_Tracker_Table.py
+++ FS_Tracker_Table.py
@@ -1,51 +1,63 @@
+import json
+
 
 class FS_Table:
     def __init__(self):
         self.contents = {}  # Inicia o nodo vazio
-        self.address = {} # Inicializa o endereço do nodo
-    
-    def __str__(self):
-        return
-    
+        # self.node_addresses = {} # Inicializa o endereço do nodo
 
-    def addNode(self, node_id, address):
+    def __str__(self):
+
+        out = ""
+        out += json.dumps(self.contents, indent=4)
+        # out += f"{self.node_addresses} \n"
+
+        return out
+
+    def __repr__(self):
+
+        out = ""
+        out += json.dumps(self.contents, indent=4)
+        # out += f"{self.node_addresses} \n"
+
+        return out
+
+    def addNode(self, node_id):
         # Adiciona um nodo ao tracker
-        self.node_id = node_id
+        # self.node_id = node_id
         self.contents[node_id] = {}
-        self.node_addresses[node_id] = address
         return node_id
 
-    def addFileFragments(self, node_id, file_id):
+    def addFile(self, node_id, file_id, fragments=[False] * 20, file_size="FILE SIZE"):
         # Adiciona um ficheiro a um nodo com os fragmentos inicializados a falso
         if node_id not in self.contents:
-            self.contents[node_id] = {}
-        fragments = [False] * 20
-        self.contents[node_id][file_id] = fragments
+            self.addNode(node_id)
+        self.contents[node_id][file_id] = [file_size,fragments]
 
-    def updateFragments(self, node_id, file_id, fragments): #incluir lista de fragmentos
+    def updateFragments(self, node_id, file_id, fragments):  # incluir lista de fragmentos
         # Dá update ao fragmento de um ficheiro para um dado estado (Tanto para adicionar como remover)
-        if self.contents[node_id][file_id]:    
+        if self.contents[node_id][file_id]:
             i = 0
             while i < 20:
-                self.contents[node_id][file_id][fragments[i]]=fragments[i]
+                self.contents[node_id][file_id][1][i] = fragments[i]
                 i += 1
-    
+
     def addCompleteNode(self, nodeId, address, fragments, fileId):
         self.addNode(nodeId, address)
         self.addFileFragments(nodeId, fileId)
         self.updateFragments(nodeId, fileId, fragments)
-    
+
     def removeFileFromNode(self, node_id, file_id):
         # Remove um ficheiro de um nodo
         if node_id in self.contents and file_id in self.contents[node_id]:
             del self.contents[node_id][file_id]
-    
+
     def removeNode(self, node_id):
         # Remove a node and all its associated information from the table
         if node_id in self.contents:
             del self.contents[node_id]
-        if node_id in self.node_addresses:
-            del self.node_addresses[node_id]
+        # if node_id in self.node_addresses:
+        #     del self.node_addresses[node_id]
 
     def getFragmentStatus(self, node_id, file_id):
         # Verifica o estado de fragmentos de um ficheiro (Para verificação manual?)
@@ -63,7 +75,7 @@ class FS_Table:
                 if 0 <= fragment_index < 20 and fragments[fragment_index]:
                     nodes_with_fragment.append(node_id)
         return nodes_with_fragment
-    
+
     def getNodesWithFilename(self, filename):
         # Verifica os nodos que têm um ficheiro
         nodes_with_filename = []
@@ -75,11 +87,11 @@ class FS_Table:
     def getContents(self):
         # Retorna a tabela de nodos
         return self.contents
-    
-    def getNodeAddress(self, node_id):
+
+    # def getNodeAddress(self, node_id):
         # Retorna o endereço de um nodo
-        return self.node_addresses.get(node_id)
-    
+        # return self.node_addresses.get(node_id)
+
     def writeTable(self):
         string = []
 
@@ -90,6 +102,5 @@ class FS_Table:
             for files in self.contents[node_id]:
                 string.append(files)
                 string.append(", ")
-        
+
         return string
-  
