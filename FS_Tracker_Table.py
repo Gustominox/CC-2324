6acad8e7e920fd6784cde9279d80bebf41f983fe
Gustominox <agstcmps.111@gmail.com>
2023-10-20 12:17:54 +0100

    comment unused methods to debug

--- FS_Tracker_Table.py
+++ FS_Tracker_Table.py
@@ -36,50 +36,50 @@ class FS_Table:
         for newFile in body:    
             self.contents[node_id][newFile] = body[newFile] 
         
-    def updateFragments(self, node_id, file_id, fragments):  # incluir lista de fragmentos
-        # Dá update ao fragmento de um ficheiro para um dado estado (Tanto para adicionar como remover)
-        if self.contents[node_id][file_id]:
-            i = 0
-            while i < 20:
-                self.contents[node_id][file_id][1][i] = fragments[i]
-                i += 1
+    # def updateFragments(self, node_id, file_id, fragments):  # incluir lista de fragmentos
+    #     # Dá update ao fragmento de um ficheiro para um dado estado (Tanto para adicionar como remover)
+    #     if self.contents[node_id][file_id]:
+    #         i = 0
+    #         while i < 20:
+    #             self.contents[node_id][file_id][1][i] = fragments[i]
+    #             i += 1
 
-    def addCompleteNode(self, nodeId, address, fragments, fileId):
-        self.addNode(nodeId, address)
-        self.addFileFragments(nodeId, fileId)
-        self.updateFragments(nodeId, fileId, fragments)
+    # def addCompleteNode(self, nodeId, address, fragments, fileId):
+    #     self.addNode(nodeId, address)
+    #     self.addFileFragments(nodeId, fileId)
+    #     self.updateFragments(nodeId, fileId, fragments)
 
-    def removeFileFromNode(self, node_id, file_id):
-        # Remove um ficheiro de um nodo
-        if node_id in self.contents and file_id in self.contents[node_id]:
-            del self.contents[node_id][file_id]
+    # def removeFileFromNode(self, node_id, file_id):
+    #     # Remove um ficheiro de um nodo
+    #     if node_id in self.contents and file_id in self.contents[node_id]:
+    #         del self.contents[node_id][file_id]
 
-    def removeNode(self, node_id):
-        # Remove a node and all its associated information from the table
-        if node_id in self.contents:
-            del self.contents[node_id]
-        # if node_id in self.node_addresses:
-        #     del self.node_addresses[node_id]
+    # def removeNode(self, node_id):
+    #     # Remove a node and all its associated information from the table
+    #     if node_id in self.contents:
+    #         del self.contents[node_id]
+    #     # if node_id in self.node_addresses:
+    #     #     del self.node_addresses[node_id]
 
-    def getFragmentStatus(self, node_id, file_id):
-        # Verifica o estado de fragmentos de um ficheiro (Para verificação manual?)
-        if node_id in self.contents and file_id in self.contents[node_id]:
-            return self.contents[node_id][file_id]
-        else:
-            return None
+    # def getFragmentStatus(self, node_id, file_id):
+    #     # Verifica o estado de fragmentos de um ficheiro (Para verificação manual?)
+    #     if node_id in self.contents and file_id in self.contents[node_id]:
+    #         return self.contents[node_id][file_id]
+    #     else:
+    #         return None
 
-    def getNodesWithFragment(self, file_id, fragment_index):
-        # Retorna os nodos que têm um fragmento de um ficheiro
-        nodes_with_fragment = []
-        for node_id, files in self.contents.items():
-            if file_id in files:
-                fragments = files[file_id]
-                if 0 <= fragment_index < 20 and fragments[fragment_index]:
-                    nodes_with_fragment.append(node_id)
-        return nodes_with_fragment
+    # def getNodesWithFragment(self, file_id, fragment_index):
+    #     # Retorna os nodos que têm um fragmento de um ficheiro
+    #     nodes_with_fragment = []
+    #     for node_id, files in self.contents.items():
+    #         if file_id in files:
+    #             fragments = files[file_id]
+    #             if 0 <= fragment_index < 20 and fragments[fragment_index]:
+    #                 nodes_with_fragment.append(node_id)
+    #     return nodes_with_fragment
 
     def getNodesWithFilename(self, body):
-        # Verifica os nodos que têm um ficheiro
+        # Verifica os nodos que têm os ficheiros referenciados na msg
         nodes_with_filename = {}
         for search_file in body:
             nodes_with_filename[search_file] = []
@@ -89,9 +89,9 @@ class FS_Table:
         
         return nodes_with_filename
 
-    def getContents(self):
-        # Retorna a tabela de nodos
-        return self.contents
+    # def getContents(self):
+    #     # Retorna a tabela de nodos
+    #     return self.contents
 
     # def getNodeAddress(self, node_id):
         # Retorna o endereço de um nodo
