ff3ea73596bf1f2618cf7669a4fbc3d54e1bac8b
Gustominox <agstcmps.111@gmail.com>
2023-10-31 10:46:12 +0000

    delimit msg, se n tiver a msg toda espera que o resto seja enviado

--- FS_Tracker.py
+++ FS_Tracker.py
@@ -19,34 +19,43 @@ class FS_Tracker:
     
     def launchTcpConnection(self,connection, address):
         
+        msg = ""
+        
         while True:
-            # TODO: delimit message, msg could be bigger than 1024 bits
+            # TODO: delimit message is done e preciso receber agora mais que uma mensagem ou seja fazer o parse de "()"
             data = connection.recv(1024)
+            logging.info("Received data")
             if data:
-                msg = data.decode('utf-8')
+                msg += data.decode('utf-8')
                 
                 message = FS_Msg()
                 fullMessage = message.read_message(msg)
                 
-                if message.MSG_TYPE == "UPDATE NODE":
-                    self.table.updateNode(message.SENDER_ID,message.BODY)
-                    logging.info(f"UPDATE: {message.SENDER_ID}")
+                if fullMessage:
+                    if message.MSG_TYPE == "UPDATE NODE":
+                        self.table.updateNode(message.SENDER_ID,message.BODY)
+                        logging.info(f"UPDATE: {message.SENDER_ID}")
 
-                elif message.MSG_TYPE == "DELETE NODE":
-                    
-                    self.table.removeNode(message.SENDER_ID)
-                    logging.info(f"REMOVE: {message.SENDER_ID}")
-                    
-                elif message.MSG_TYPE == "ASK FILE":
-                    
-                    node_list = self.table.getNodesWithFilename(message.BODY)
-                    print(f"NODE LIST: \n{node_list}")
-                    connection.send(f"{node_list}".encode('utf-8'))
-                    
-                elif message.MSG_TYPE == "END TRACKER":
-                    pass
+                    elif message.MSG_TYPE == "DELETE NODE":
+                        
+                        self.table.removeNode(message.SENDER_ID)
+                        logging.info(f"REMOVE: {message.SENDER_ID}")
+                        
+                    elif message.MSG_TYPE == "ASK FILE":
+                        
+                        node_list = self.table.getNodesWithFilename(message.BODY)
+                        print(f"NODE LIST: \n{node_list}")
+                        connection.send(f"{node_list}".encode('utf-8'))
+                        
+                    elif message.MSG_TYPE == "END TRACKER":
+                        pass
+                    else:
+                        logging.error(f"INVALID MESSAGE FROM NODE: {message.MSG_TYPE}")
+                    # clean msg
+                    msg = ""
                 else:
-                    logging.error(f"INVALID MESSAGE FROM NODE: {message.MSG_TYPE}")
+                    pass
+                    
             else:
                 logging.info(f"Closing connection {address}")
                 break
