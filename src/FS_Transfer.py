8db8aa531cac51dc2730186a8ea572d3193fd783
Gustominox <agstcmps.111@gmail.com>
2023-11-22 14:57:47 +0000

    Resolver bug de import circular

--- src/FS_Transfer.py
+++ src/FS_Transfer.py
@@ -1,6 +1,22 @@
 import struct
 import time
-from FS_Mediator import FRAG_SIZE, HASH_SIZE, FRAG_INDEX_SIZE, PAYLOAD, PAYLOAD_BYTES, HEADER_MAX
+
+# from FS_Mediator import FRAG_SIZE, HASH_SIZE, FRAG_INDEX_SIZE, PAYLOAD, PAYLOAD_BYTES, HEADER_MAX
+
+#Tamanhos dos componentes dos headers
+ASK_SEND_FLAG = 2
+HASH_SIZE = 32
+FRAG_INDEX_SIZE = 32
+FRAG_SIZE = 32
+TIME_SIZE = 64
+PAYLOAD=1024
+#header de pedido para receber fragmento: Composto por 2bitflag, hash, fragIndex, fragSize + fill até 136 bits (38 bits fill)
+HEADER_RECV = ASK_SEND_FLAG + HASH_SIZE + FRAG_INDEX_SIZE + FRAG_SIZE
+#header de pedido para enviar fragmento: COmposto por 2bitflag, hash, fragIndex, timeSince + fill até 136 bits (6 bits fill)
+HEADER_PING = ASK_SEND_FLAG + HASH_SIZE + FRAG_INDEX_SIZE + TIME_SIZE
+HEADER_MAX = max(HEADER_PING,HEADER_RECV) + (8 - (max(HEADER_PING,HEADER_RECV) % 8)) #136 bits
+PACKET_SIZE = HEADER_MAX + PAYLOAD
+PAYLOAD_BYTES = (PAYLOAD + 7) // 8
 
 class FS_transfer:
 
